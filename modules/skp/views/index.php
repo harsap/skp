6feb1b4f84c32ac3f39b5c76db0cfa0007e861a5
Galih Pratama <hanamura.iost@gmail.com>
2014-09-22 13:05:42 +0700

    Add New Modal Dialog for "Pilih Pegawai"

--- modules/skp/views/index.php
+++ modules/skp/views/index.php
@@ -1,28 +1,59 @@
 <ul class="page-breadcrumb breadcrumb">
     <li>
         <i class="icon-home"></i>
-        <a href="<?php echo base_url(); ?>">Home</a> 
+        <a href="<?php echo base_url(); ?>">Home</a>
         <i class="icon-angle-right"></i>
-    </li>     
+    </li>
     <li>
         Pembuatan SKP
     </li>
 </ul>
 <div class="row">
 	<form action="#" method="post">
-	<div class="col-sm-12">	    
+	<div class="col-sm-12">
 	    <h3>
 	    	Form Entry Formulir Sasaran Kinerja Pegawai (SKP)
-	    	<a type="button" class="btn btn-md btn-primary pull-right"><i class="glyphicon glyphicon-user"></i> Pilih Pegawai Yang Akan Dinilai</a>
+	    	<button type="button" data-toggle="modal" data-target="#pilih_pegawai" class="btn btn-md btn-primary pull-right"><i class="glyphicon glyphicon-user"></i> Pilih Pegawai Yang Akan Dinilai</button>
 	    </h3><br>
+            <!-- Modal -->
+            <div class="modal fade" id="pilih_pegawai" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Pilih Pegawai</h4>
+                  </div>
+                  <div class="modal-body">
+                    <select name="select_pegawai_dropdown" width="350px" class="pegawai-select col-sm-12">
+                    <?php
+                      foreach ($list_pegawai->result() as $list_pegawai_result) {  ?>
+                        <option value="<?php echo $list_pegawai_result->peg_nip_baru ?>"><?php echo $list_pegawai_result->peg_nama ?> (<?php echo $list_pegawai_result->peg_nip_baru ?>)</option>
+                      <?php } ?>
+                    </select>
+
+                    <script charset="utf-8">
+                    $(window).load(function(){
+                       $(".pegawai-select").chosen();
+                     });
+                    </script>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
 	    	<div class='col-sm-6'>
-	    		<?php $user = $this->ion_auth->get_data_user_by_id(); 
+	    		<?php $user = $this->ion_auth->get_data_user_by_id();
 	    			  $listuser = $this->ion_auth->get_user_name_all();
 	    		?>
 	    		<table class="col-sm-12 table table-bordered" id="tblPenilai">
 	    			<tr>
 	    			   	<th width="10%">No</th>
-	    			   	<th colspan="2">I. Pejabat Penilai</th>	    		   	
+	    			   	<th colspan="2">I. Pejabat Penilai</th>
 	    			</tr>
 	    			<tr>
 	    				<td>1</td>
@@ -49,14 +80,14 @@
 	    			    <td>Unit Kerja</td>
 	    			    <td><?php echo $user->unit_kerja_nama; ?></td>
 	    			</tr>
-	    		</table>  
+	    		</table>
 	    	</div>
-	    	<div class='col-sm-6'>	    	
+	    	<div class='col-sm-6'>
 	    		<table class="col-sm-12 table table-bordered" id="tblDinilai">
 	    			<tr>
 	    			   	<th width="10%">No</th>
 	    			   	<th colspan="2">II. Pegawai Negeri Sipil Yang Dinilai</th>
-	    			   	
+
 	    			</tr>
 	    			<tr>
 	    				<td>1</td>
@@ -83,16 +114,16 @@
 	    			    <td>Unit Kerja</td>
 	    			    <td id="dinilaiUK"></td>
 	    			</tr>
-	    		</table>  
+	    		</table>
 	   		</div>
 	</div>
 </div>
 <div class='row'>
-	<div class='col-sm-12'>		
+	<div class='col-sm-12'>
 	    <h3>
 	    	Kegiatan Tugas Pokok Jabatan
 	    </h3>
-	    <hr>			    
+	    <hr>
 			<table class="table table-bordered table-stripped" id="tblJabatan">
 			  <tr>
 			 	<th colspan="7">
@@ -100,11 +131,11 @@
 				 		<button onclick="addRowToTable();" type="reset" class="btn btn-success btn-sm "><i class="glyphicon glyphicon-plus"></i> Tambah Baris Baru</button>
 				 		<a onclick="removeRowFromTable();" type="reset" class="btn btn-warning btn-sm "><i class="glyphicon glyphicon-minus"></i> Hapus Baris</a>
 			 		</div>
-			 	</th>	
+			 	</th>
 			  </tr>
 			  <tr>
-			    <th rowspan="2">No.</th>
-			    <th rowspan="2" width="50%">Kegiatan Tugas Pokok Jabatan</th>
+			    <th width="1%" rowspan="2">No.</th>
+			    <th rowspan="2" width="50%">Kegiatan Tugaas Pokok Jabatan</th>
 			    <th rowspan="2">AK</th>
 			    <th colspan="4">Target</th>
 			  </tr>
@@ -116,12 +147,12 @@
 			  </tr>
 			  <tr>
 			    <td>1</td>
-			    <td><input type="text" class="form-control col-sm-12"></td>
-			    <td><input type="text" class="form-control col-sm-12"></td>
-			    <td><input type="text" class="form-control col-sm-12"></td>
-			    <td><input type="text" class="form-control col-sm-12"></td>
-			    <td><input type="text" class="form-control col-sm-12"></td>
-			    <td><input type="text" class="form-control col-sm-12"></td>
+			    <td><input type="text" name="" class="form-control col-sm-12"></td>
+			    <td><input type="text" name="" class="form-control col-sm-12"></td>
+			    <td><input type="text" name="" class="form-control col-sm-12"></td>
+			    <td><input type="text" name="" class="form-control col-sm-12"></td>
+			    <td><input type="text" name="" class="form-control col-sm-12"></td>
+			    <td><input type="text" name="" class="form-control col-sm-12"></td>
 			  </tr>
 			</table>
 	</div>
@@ -135,10 +166,10 @@
 				 		<button onclick="addRowToTable2();" type="reset" class="btn btn-success btn-sm "><i class="glyphicon glyphicon-plus"></i> Tambah Baris Baru</button>
 				 		<a onclick="removeRowFromTable2();" type="reset" class="btn btn-warning btn-sm "><i class="glyphicon glyphicon-minus"></i> Hapus Baris</a>
 			 		</div>
-			 	</th>	
+			 	</th>
 			  </tr>
 			  <tr>
-			    <th rowspan="2">No.</th>
+			    <th width="20px" rowspan="2">No.</th>
 			    <th rowspan="2" width="50%">Kegiatan Tugas Pokok Tambahan</th>
 			    <th rowspan="2">AK</th>
 			    <th colspan="4">Target</th>
@@ -150,7 +181,7 @@
 			    <td>Biaya</td>
 			  </tr>
 			  <tr>
-			    <td>1</td>
+			    <td><input type="text" class="form-control col-sm-12" name="nomor_kegiatan_1" value="1"></td>
 			    <td><input type="text" class="form-control col-sm-12"></td>
 			    <td><input type="text" class="form-control col-sm-12"></td>
 			    <td><input type="text" class="form-control col-sm-12"></td>
@@ -179,20 +210,24 @@
 	  // if there's no header row in the table, then iteration = lastRow + 1
 	  var iteration = lastRow - 2;
 	  var row = tbl.insertRow(lastRow);
-	  
+
 	  // left cell
 	  var cellOne = row.insertCell(0);
-	  var textNode = document.createTextNode(iteration);
-	  
-	  cellOne.appendChild(textNode);
-	  
+    var el = document.createElement('input');
+    el.type = 'text';
+    el.name = 'nomor_kegiatan_' + iteration;
+    el.value = iteration;
+    el.id = 'txtRow';
+
+	  cellOne.appendChild(el);
+
 	  // right cell
 	  var cellTwo = row.insertCell(1);
 	  var el = document.createElement('input');
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellTwo.appendChild(el);
 
 	  var cellThree = row.insertCell(2);
@@ -200,7 +235,7 @@
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellThree.appendChild(el);
 
 
@@ -209,7 +244,7 @@
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellFour.appendChild(el);
 
 
@@ -218,7 +253,7 @@
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellFive.appendChild(el);
 
 
@@ -227,7 +262,7 @@
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellSix.appendChild(el);
 
 
@@ -236,10 +271,10 @@
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellSeven.appendChild(el);
 
-	  
+
 	}
 
 	function removeRowFromTable()
@@ -256,20 +291,24 @@
 	  // if there's no header row in the table, then iteration = lastRow + 1
 	  var iteration = lastRow - 2;
 	  var row = tbl.insertRow(lastRow);
-	  
+
 	  // left cell
-	  var cellOne = row.insertCell(0);
-	  var textNode = document.createTextNode(iteration);
-	  
-	  cellOne.appendChild(textNode);
-	  
+    var cellOne = row.insertCell(0);
+    var el = document.createElement('input');
+    el.type = 'text';
+    el.name = 'nomor_kegiatan_' + iteration;
+    el.value = iteration;
+    el.id = 'txtRow';
+
+    cellOne.appendChild(el);
+
 	  // right cell
 	  var cellTwo = row.insertCell(1);
 	  var el = document.createElement('input');
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellTwo.appendChild(el);
 
 	  var cellThree = row.insertCell(2);
@@ -277,7 +316,7 @@
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellThree.appendChild(el);
 
 
@@ -286,7 +325,7 @@
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellFour.appendChild(el);
 
 
@@ -295,7 +334,7 @@
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellFive.appendChild(el);
 
 
@@ -304,7 +343,7 @@
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellSix.appendChild(el);
 
 
@@ -313,10 +352,10 @@
 	  el.type = 'text';
 	  el.name = 'txtRow' + iteration;
 	  el.id = 'txtRow';
-	  
+
 	  cellSeven.appendChild(el);
 
-	  
+
 	}
 
 	function removeRowFromTable2()
