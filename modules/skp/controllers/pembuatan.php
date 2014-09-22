dbfda8b5012bb6e1fecf63f78283cb23875853f8
Galih Pratama <hanamura.iost@gmail.com>
2014-09-22 13:54:58 +0700

    Call get_pegawai model and initialize peg_id from URI

--- modules/skp/controllers/pembuatan.php
+++ modules/skp/controllers/pembuatan.php
@@ -15,8 +15,10 @@ class pembuatan extends CI_Controller{
         $this->ion_auth->check_uri_permissions();
         $this->load->model('skp_model', 'skp_model');
     }
-    public function index() {
+    public function index($peg_id = null) {
       $data['list_pegawai'] = $this->skp_model->get_all_pegawai();
+      $data['pegawai'] = $this->skp_model->get_pegawai($peg_id);
+      $data['peg_id'] = $this->uri->segment(4);
         // $data['listgol'] = $this->personilmodel->getallgolongan();
         // $data['listeselon'] = $this->personilmodel->geteselon();
         $this->template->load('mainlayout', 'index', $data);
